26b82cb9376fb7020758340dfaa7fe73c5d961ba
Valdeci Gomes <coderade@outlook.com>
2017-08-20 11:09:59 -0300

    Update Member List component to add some dummy data.

--- src/components/ui/MemberList.js
+++ src/components/ui/MemberList.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { Component } from 'react'
+import {Component} from 'react'
+
 // import fetch from 'isomorphic-fetch'
 
 class MemberList extends Component {
     render() {
         return (
             <div className="member-list">
-            	<h1>Society Members</h1>
+                <h1>Society Members</h1>
+                <ul>
+                    <li>Member 1</li>
+                    <li>Member 2</li>
+                    <li>Member 3</li>
+                    <li>Member 4</li>
+                </ul>
             </div>
-        )    
-   }     
+        )
+    }
 }
 
 export default MemberList;
